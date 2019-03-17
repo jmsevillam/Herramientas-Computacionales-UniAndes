a4b8c8b1f01df928422c87719dfd6de2e583a890
jmsevillam <jmsevillam@unal.edu.co>
2019-03-16 20:19:53 -0500

    Title corrected

---
+++ Homework/Hw6/Solution/calc.py
@@ -0,0 +1,25 @@
+class calculator:
+    '''
+    This class is a basic calculator, it
+    sums, substracts, multiply, divide and exponentiates.
+    '''
+    def sum(self,a,b):
+        a=float(a)
+        b=float(b)
+        return a+b
+    def sub(self,a,b):
+        a=float(a)
+        b=float(b)
+        return a-b
+    def div(self,a,b):
+        a=float(a)
+        b=float(b)
+        return a/b
+    def mult(self,a,b):
+        a=float(a)
+        b=float(b)
+        return a*b
+    def pow(self,a,b):
+        a=float(a)
+        b=float(b)
+        return a**b
